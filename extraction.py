00af59c514b8a0c5b0a9c180bc467a78cae2e256
Yuri Petkevich <yuripetkevich@gmail.com>
2018-05-06 21:52:04 +0300

    Modify methods to get s-a-o. Now works with SABR root type

--- extraction.py
+++ extraction.py
@@ -36,6 +36,21 @@ def move_to_last_vp(tree):
 
 
 def find_subject(tree):
+    if tree.label() in ['SBAR']:
+        node = tree.parent()
+        return get_subject(node)
+    else:
+        return get_subject(tree)
+
+        # for child in tree:
+        #     if child.label() in ['NP', 'PP']:
+        #         node = move_to_last_np(child)
+        #         for sibling in node:
+        #             if sibling.label() in ['NN', 'NNS', 'PRP', 'NNP', 'NNPS']:
+        #                 return sibling[0]
+
+
+def get_subject(tree):
     for child in tree:
         if child.label() in ['NP', 'PP']:
             node = move_to_last_np(child)
@@ -45,12 +60,47 @@ def find_subject(tree):
 
 
 def find_action(tree):
+    if tree.label() in ['SBAR']:
+        for sub in tree:
+            if sub.label() in ['S']:
+                return get_action(sub)
+    else:
+        return get_action(tree)
+        # for child in tree:
+        #     if child.label() in ['VP']:
+        #         node = move_to_last_vp(child)
+        #         for sibling in node:
+        #             if sibling.label() in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
+        #                 return sibling[0]
+
+
+def get_action(tree):
     for child in tree:
         if child.label() in ['VP']:
             node = move_to_last_vp(child)
             for sibling in node:
                 if sibling.label() in ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']:
                     return sibling[0]
+
+
+def find_object(tree):
+    if tree.label() in ['SBAR']:
+        for sub in tree:
+            if sub.label() in ['S']:
+                return get_object(sub)
+    else:
+        return get_object(tree)
+    # for child in tree:
+    #     if child.label() in ['VP']:
+    #         node = move_to_last_vp(child)
+    #         return find_subject(node)
+
+
+def get_object(tree):
+    for child in tree:
+        if child.label() in ['VP']:
+            node = move_to_last_vp(child)
+            return find_subject(node)
 
 
 def find_subject_np_sbar(tree):
@@ -78,21 +128,46 @@ def check_np_vp_in_s(tree):
     return False
 
 
-def find_object(tree):
-    for child in tree:
-        if child.label() in ['VP']:
-            node = move_to_last_vp(child)
-            return find_subject(node)
+def get_sibling_labels(tree):
+    labels = []
+    for sibling in tree:
+        labels.append(sibling.label())
+
+    return labels
 
 
 def find_trees_to_analyze(tree):
-    trees = [tree]
-    for sibling in tree.subtrees():
-        if sibling.label() in ['NP']:
-            for kid in sibling.subtrees():
-                if kid.label() in ['VP']:
-                    trees.append(sibling)
+    trees = []
+    q = Queue.LifoQueue()
+    q.put(tree)
+    while not q.empty():
+        cur_tree = q.get()
+        if check_tree(cur_tree):
+            trees.append(cur_tree)
+
+        for sub in cur_tree:
+            if not isinstance(sub[0], unicode):
+                q.put(sub)
+
     return trees
+
+
+def check_tree(tree):
+    labels = get_sibling_labels(tree)
+    # print labels
+    if ('NP' in labels) and ('VP' in labels):
+        return True
+    elif ('WHNP' in labels) and ('S' in labels):
+        return True
+    else:
+        return False
+
+        # trees = [tree]
+        # for sibling in tree.subtrees():
+        #     if sibling.label() in ['NP']:
+        #         for kid in sibling.subtrees():
+        #             if kid.label() in ['VP']:
+        #                 trees.append(sibling)
 
 
 def find_triple(tree):
@@ -108,13 +183,11 @@ def find_triples(sentence):
     t = get_tree(sentence)
     trees = find_trees_to_analyze(t)
     for tree in trees:
+        tree.pretty_print()
         svo = find_triple(tree)
         triples.append(svo)
 
     return triples
-
-
-
 
 # for sent in sentences:
 #     t = get_tree(sent)
