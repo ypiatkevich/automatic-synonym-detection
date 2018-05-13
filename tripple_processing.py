a8b1f9c81db22d4935147e5374723e3bbc9f07dd
Yuri Petkevich <yuripetkevich@gmail.com>
2018-05-13 19:08:47 +0300

    Add functionality to get verb object candidates

--- tripple_processing.py
+++ tripple_processing.py
@@ -2,12 +2,20 @@ import numpy as np
 import itertools
 
 
-def filter_triples(triples):
-    return [triple for triple in triples if triple.valid()]
+def s_v_filter(triples):
+    return [triple for triple in triples if triple.s_v_valid()]
+
+
+def v_o_filter(triples):
+    return [triple for triple in triples if triple.v_o_valid()]
+
+
+def s_v_o_filter(triples):
+    return [triple for triple in triples if triple.s_v_o_valid()]
 
 
 def subject_verb_frequency_matrix(triples):
-    filtered_triples = filter_triples(triples)
+    filtered_triples = s_v_filter(triples)
     verbs = get_verbs(filtered_triples)
     subjects = get_subjects(filtered_triples)
     matrix = np.zeros((len(verbs), len(subjects)))
@@ -19,16 +27,50 @@ def subject_verb_frequency_matrix(triples):
     return matrix
 
 
+def subject_verb_id_matrix(triples):
+    filtered_triples = s_v_filter(triples)
+    verbs = get_verbs(filtered_triples)
+    subjects = get_subjects(filtered_triples)
+    matrix = []
+    for i in range(len(verbs)):
+        row = []
+        for j in range(len(subjects)):
+            el = []
+            row.append(el)
+
+        matrix.append(row)
+
+    for triple in filtered_triples:
+        v_ind = verbs.index(triple.action)
+        s_ind = subjects.index(triple.subject)
+        matrix[v_ind][s_ind].append(triple.id)
+
+    return matrix
+
+
+def verb_object_frequency_matrix(triples):
+    filtered_triples = v_o_filter(triples)
+    verbs = get_verbs(filtered_triples)
+    objects = get_objects(filtered_triples)
+    matrix = np.zeros((len(verbs), len(objects)))
+    for triple in filtered_triples:
+        v_ind = verbs.index(triple.action)
+        o_ind = objects.index(triple.object)
+        matrix[v_ind][o_ind] += 1
+
+    return matrix
+
+
 def get_subjects(triples):
-    return list(set(triple.subject for triple in filter_triples(triples)))
+    return list(set(triple.subject for triple in triples))
 
 
 def get_verbs(triples):
-    return list(set(triple.action for triple in filter_triples(triples)))
+    return list(set(triple.action for triple in triples))
 
 
 def get_objects(triples):
-    return list(set(triple.object for triple in filter_triples(triples)))
+    return list(set(triple.object for triple in triples))
 
 
 def get_candidates(matrix):
@@ -57,4 +99,9 @@ def get_filtered_candidates(candidates, min):
 
 def print_subject_candidate_names(candidate, triples):
     subjects = get_subjects(triples)
+    print '{0} - {1}'.format(subjects[candidate[0]], subjects[candidate[1]])
+
+
+def print_object_candidate_names(candidate, triples):
+    subjects = get_objects(triples)
     print '{0} - {1}'.format(subjects[candidate[0]], subjects[candidate[1]])
