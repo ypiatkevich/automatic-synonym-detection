54e42fb31e19cb084b46f68066aff3b3082f4281
Yuri Petkevich <yuripetkevich@gmail.com>
2018-05-13 11:13:26 +0300

    Add method to read text from file and method to write triples to file

--- corpora_processing.py
+++ corpora_processing.py
@@ -1,3 +1,23 @@
+import os
+
+from text_processing import *
+
+
 def read_file(file):
     file = open(file, "r")
     return [line for line in file.readlines() if line != '\n']
+
+
+def read_text(file):
+    file = open(file, "r")
+    text = file.read()
+    return split_into_sentences(text)
+
+
+def write_triples_to_file(triples):
+    open("output/triples.txt", "w").close()
+    file = open("output/triples.txt", "r+")
+    for triple in triples:
+        file.write(triple.__str__())
+    file.close()
+
